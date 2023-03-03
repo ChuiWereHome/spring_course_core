0ec92443304835742c33cbaf9258f17df39cabd8
zerkalov <n.zerkalov@cft.ru>
2023-03-04 01:53:56 +0700

    first lesson: create bean

---
+++ src/main/java/ru/zerkalov/springcourse/Harry.java
@@ -0,0 +1,14 @@
+package ru.zerkalov.springcourse;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Harry {
+    private String name;
+}
--- src/main/java/ru/zerkalov/springcourse/Main.java
+++ src/main/java/ru/zerkalov/springcourse/Main.java
@@ -1,7 +1,16 @@
 package ru.zerkalov.springcourse;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
+        var a = context.getBean("harry", Harry.class);
+        System.out.println("Name --> " + a.getName());
     }
 }
