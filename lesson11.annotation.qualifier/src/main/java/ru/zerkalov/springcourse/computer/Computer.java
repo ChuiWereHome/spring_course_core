708d4cef337f42ed4996734e3980f4d1fa2cadc9
zerkalov <n.zerkalov@cft.ru>
2023-03-04 22:00:46 +0700

    update lesson11

--- lesson11.annotation.qualifier/src/main/java/ru/zerkalov/springcourse/computer/Computer.java
+++ lesson11.annotation.qualifier/src/main/java/ru/zerkalov/springcourse/computer/Computer.java
@@ -1,11 +1,14 @@
 package ru.zerkalov.springcourse.computer;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.zerkalov.springcourse.GenresMusic;
 import ru.zerkalov.springcourse.player.CdPlayer;
-
+@Getter
+@Setter
 @Component
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class Computer {
--- lesson11.annotation.qualifier/src/main/java/ru/zerkalov/springcourse/player/CdPlayer.java
+++ lesson11.annotation.qualifier/src/main/java/ru/zerkalov/springcourse/player/CdPlayer.java
@@ -1,23 +1,32 @@
 package ru.zerkalov.springcourse.player;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ru.zerkalov.springcourse.GenresMusic;
 import ru.zerkalov.springcourse.music.Music;
 
 import java.util.Random;
 
+@Getter
+@Setter
 @Component
 public class CdPlayer {
+
+    private final int serialNumber;
     private final Music danceMusic1;
     private final Music tripHopMusic1;
 
     @Autowired
     public CdPlayer(@Qualifier("danceMusic") Music danceMusic1,
-                    @Qualifier("tripHopMusic") Music tripHopMusic1) {
+                    @Qualifier("tripHopMusic") Music tripHopMusic1,
+                    @Value("${cd.player.serial.number}") int serialNumber) {
         this.danceMusic1 = danceMusic1;
         this.tripHopMusic1 = tripHopMusic1;
+        this.serialNumber = serialNumber;
     }
 
     public void play(GenresMusic genres) {
