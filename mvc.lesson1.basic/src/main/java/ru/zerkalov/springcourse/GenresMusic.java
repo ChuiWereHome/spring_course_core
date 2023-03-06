272ea3944fb3af6c7486a76d7c72344878b0375e
nikolayzerkalov <nikolayzerkalov@gmail.com>
2023-03-06 22:25:13 +0700

    remove mvc.lesson1.basic

--- mvc.lesson1.basic/src/main/java/ru/zerkalov/springcourse/GenresMusic.java
+++
@@ -1,6 +0,0 @@
-package ru.zerkalov.springcourse;
-
-public enum GenresMusic {
-    TRIP_HOP_MUSIC,
-    DANCE_MUSIC
-}
--- mvc.lesson1.basic/src/main/java/ru/zerkalov/springcourse/music/Music.java
+++
@@ -1,7 +0,0 @@
-package ru.zerkalov.springcourse.music;
-
-import java.util.List;
-
-public interface Music {
-    List<String> getPlayList();
-}
